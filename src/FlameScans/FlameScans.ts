15f009eab816b01ef1641b5dd3408b6d5506c274
pandeynmn <namanpandey23@gmail.com>
2022-01-18 22:41:14 -0700

    Flame Scans

---
+++ src/FlameScans/FlameScans.ts
@@ -0,0 +1,195 @@
+import {
+    Chapter,
+    ChapterDetails,
+    ContentRating,
+    HomeSection,
+    Manga,
+    MangaTile,
+    PagedResults,
+    SearchRequest,
+    Source,
+    SourceInfo,
+    TagSection,
+    TagType,
+} from 'paperback-extensions-common'
+
+import { Parser } from './parser'
+
+const FS_DOMAIN = 'https://flamescans.org'
+
+export const FlameScansInfo: SourceInfo = {
+    version: '2.0.0',
+    name: 'FlameScans',
+    description: 'Extension that pulls manga from Flame Scans.',
+    author: 'NmN',
+    authorWebsite: 'http://github.com/pandeynmm',
+    icon: 'icon.ico',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: FS_DOMAIN,
+    sourceTags: [
+        {
+            text: 'English',
+            type: TagType.GREY,
+        },
+    ],
+}
+
+export class FlameScans extends Source {
+    requestManager = createRequestManager({
+        requestsPerSecond: 3,
+    })
+
+    parser = new Parser()
+
+    override getMangaShareUrl(mangaId: string): string {
+        return `${FS_DOMAIN}/series/${mangaId}`
+    }
+
+    async getMangaDetails(mangaId: string): Promise<Manga> {
+        const request = createRequestObject({
+            url: `${FS_DOMAIN}/series/${mangaId}`,
+            method: 'GET',
+        })
+        const response = await this.requestManager.schedule(request, 3)
+        const $ = this.cheerio.load(response.data)
+        return this.parser.parseMangaDetails($, mangaId)
+    }
+
+    async getChapters(mangaId: string): Promise<Chapter[]> {
+        const request = createRequestObject({
+            url: `${FS_DOMAIN}/comic/${mangaId}`,
+            method: 'GET',
+        })
+
+        const response = await this.requestManager.schedule(request, 3)
+        const $ = this.cheerio.load(response.data)
+        return this.parser.parseChapters($, mangaId, this)
+    }
+
+    async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+        const request = createRequestObject({
+            url: chapterId,
+            method: 'GET',
+        })
+
+        const response = await this.requestManager.schedule(request, 3)
+        const $ = this.cheerio.load(response.data)
+        return this.parser.parseChapterDetails($, mangaId, chapterId)
+    }
+
+    // override async getTags(): Promise<TagSection[]> {
+    //     const request = createRequestObject({
+    //         url: `${FS_DOMAIN}/search`,
+    //         method: 'GET',
+    //     })
+
+    //     const response = await this.requestManager.schedule(request, 1)
+    //     const $ = this.cheerio.load(response.data)
+    //     return this.parser.parseTags($)
+    // }
+
+    // override async supportsTagExclusion(): Promise<boolean> {
+    //     return true
+    // }
+
+    async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
+        let page = metadata?.page ?? 1
+        if (page == -1) return createPagedResults({ results: [], metadata: { page: -1 } })
+
+        // const param = `/?page=${page}${this.addTags(query)}&title=${query.title}`
+        const param = `/page/${page}/?s=${(query.title ?? '').replace(/\s/g, '+')}`
+        const request = createRequestObject({
+            url: `${FS_DOMAIN}`,
+            method: 'GET',
+            param,
+        })
+
+        const data = await this.requestManager.schedule(request, 2)
+        const $ = this.cheerio.load(data.data)
+        const manga = this.parser.parseSearchResults($)
+
+        page++
+        if (manga.length < 10) page = -1
+
+        return createPagedResults({
+            results: manga,
+            metadata: { page: page },
+        })
+    }
+
+    override async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+        const request = createRequestObject({
+            url: `${FS_DOMAIN}`,
+            method: 'GET',
+        })
+        const response = await this.requestManager.schedule(request, 2)
+        const $ = this.cheerio.load(response.data)
+
+        this.parser.parseHomeSections($, sectionCallback)
+    }
+
+    override async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+        let page = metadata?.page ?? 1
+        if (page == -1) return createPagedResults({ results: [], metadata: { page: -1 } })
+        let url = ''
+        if      (homepageSectionId == '2') url = `${FS_DOMAIN}/series/?page=${page}&order=update`
+        else if (homepageSectionId == '3') url = `${FS_DOMAIN}/series/?page=${page}?status=&type=&order=popular`
+        const request = createRequestObject({
+            url,
+            method: 'GET',
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        const $ = this.cheerio.load(response.data)
+        const manga: MangaTile[] = this.parser.parseViewMore($)
+
+        page++
+        if (manga.length < 10) page = -1
+
+        return createPagedResults({
+            results: manga,
+            metadata: { page: page },
+        })
+    }
+
+    /**
+     * Parses a time string from a Madara source into a Date object.
+     * Copied from Madara.ts made by gamefuzzy
+     */
+    protected convertTime(timeAgo: string): Date {
+        let time: Date
+        let trimmed = Number((/\d*/.exec(timeAgo) ?? [])[0])
+        trimmed = trimmed == 0 && timeAgo.includes('a') ? 1 : trimmed
+        if (timeAgo.includes('mins') || timeAgo.includes('minutes') || timeAgo.includes('minute')) {
+            time = new Date(Date.now() - trimmed * 60000)
+        } else if (timeAgo.includes('hours') || timeAgo.includes('hour')) {
+            time = new Date(Date.now() - trimmed * 3600000)
+        } else if (timeAgo.includes('days') || timeAgo.includes('day')) {
+            time = new Date(Date.now() - trimmed * 86400000)
+        } else if (timeAgo.includes('year') || timeAgo.includes('years')) {
+            time = new Date(Date.now() - trimmed * 31556952000)
+        } else {
+            time = new Date(timeAgo)
+        }
+
+        return time
+    }
+
+    // addTags(query: SearchRequest): string {
+    //     let tag_str = ''
+    //     if (query.includedTags?.length != null) {
+    //         tag_str = '&genre_inc='
+    //         for (const tag of query.includedTags) {
+    //             tag_str += `${tag.id},`
+    //         }
+    //     }
+
+    //     if (query.excludedTags?.length != null) {
+    //         tag_str += '&genre_exc='
+    //         for (const tag of query.excludedTags) {
+    //             tag_str += `${tag.id},`
+    //         }
+    //     }
+    //     return tag_str.replace(/,\s*$/, '')
+    // }
+}
---
+++ src/FlameScans/parser.ts
@@ -0,0 +1,219 @@
+import {
+    Chapter,
+    ChapterDetails,
+    HomeSection,
+    HomeSectionType,
+    LanguageCode,
+    Manga,
+    MangaStatus,
+    MangaTile,
+    Tag,
+    TagSection,
+} from 'paperback-extensions-common'
+
+export class Parser {
+    parseMangaDetails($: CheerioStatic, mangaId: string): Manga {
+        const title  = $('.thumb img').attr('alt') ?? ''
+        const image  = $('.thumb img').attr('src') ?? ''
+        const desc   = $('.entry-content.entry-content-single').text().trim() ?? ''
+        const rating = Number($('div.extra-info div.mobile-rt div.numscore').html() ?? '0')
+        let   status = MangaStatus.UNKNOWN, author = '', artist = ''
+
+        for (const obj of $('.left-side .imptdt').toArray()) {
+            const item = $('i' , obj).text().trim()
+            const type = $('h1', obj).text().trim()
+            if      (type.toLowerCase().includes('status')) status = this.mangaStatus(item.toLowerCase())
+            else if (type.toLowerCase().includes('author')) author = item
+            else if (type.toLowerCase().includes('artist')) artist = item
+        }
+
+        const arrayTags: Tag[] = []
+        for (const obj of $('.mgen a').toArray()) {
+            const id    = $(obj).attr('href')?.replace('https://flamescans.org/genres/', '').replace('/', '') ?? ''
+            const label = $(obj).text().trim()
+            if (!id || !label) continue
+            arrayTags.push({ id: id, label: label })
+        }
+        const tagSections: TagSection[] = [createTagSection({ id: '0', label: 'genres', tags: arrayTags.map((x) => createTag(x)) })]
+
+        return createManga({
+            id: mangaId,
+            titles: [this.encodeText(title)],
+            image,
+            rating: Number(rating) ?? 0,
+            status,
+            artist,
+            author,
+            tags: tagSections,
+            desc: this.encodeText(desc),
+        })
+    }
+
+    mangaStatus(str: string) {
+        if (str.includes('ongoing'))   return MangaStatus.ONGOING
+        if (str.includes('complete'))  return MangaStatus.COMPLETED
+        if (str.includes('haitus'))    return MangaStatus.HIATUS
+        if (str.includes('cancelled')) return MangaStatus.ABANDONED
+        if (str.includes('coming'))    return MangaStatus.ONGOING
+        return MangaStatus.ONGOING
+    }
+
+    parseChapters($: CheerioStatic, mangaId: string, source: any): Chapter[] {
+        const chapters: Chapter[] = []
+        const arrChapters = $('#chapterlist li').toArray().reverse()
+        for (const item of arrChapters) {
+            const id = $('a', item).attr('href') ?? ''
+            const chapNum = Number($(item).attr('data-num') ?? '0')
+
+            const time = source.convertTime($('.chapterdate', item).text().trim())
+            chapters.push(
+                createChapter({
+                    id,
+                    mangaId,
+                    name: `Chapter ${chapNum.toString()}`,
+                    chapNum,
+                    time,
+                    langCode: LanguageCode.ENGLISH,
+                })
+            )
+        }
+        return chapters
+    }
+
+    parseChapterDetails($: CheerioStatic, mangaId: string, id: string): ChapterDetails {
+        const pages: string[] = []
+
+        const chapterList = $('#readerarea p img').toArray()
+        for (const obj of chapterList) {
+            const imageUrl = $(obj).attr('src')
+            if (!imageUrl) continue
+            pages.push(imageUrl.trim())
+        }
+
+        return createChapterDetails({
+            id,
+            mangaId,
+            pages,
+            longStrip: true,
+        })
+    }
+
+    parseTags($: CheerioSelector): TagSection[] {
+        const genres: Tag[] = []
+        let i = 0
+        for (const obj of $('.col-6.col-md-4.col-lg-3.col-xl-2').toArray()) {
+            const label = $('.custom-control-label', $(obj)).text()
+            const id = $('.custom-control-input.type3', $(obj)).attr('value') ?? '29'
+            if (id == '29') i = 1
+            if (i == 0) continue
+            genres.push(createTag({ label: label, id: id }))
+        }
+        return [createTagSection({ id: '0', label: 'genres', tags: genres })]
+    }
+
+    parseSearchResults($: CheerioSelector): MangaTile[] {
+        const results: MangaTile[] = []
+
+        for (const item of $('.listupd .bsx').toArray()) {
+            const id    = $('a', item).attr('href')?.replace('https://flamescans.org/series/', '').replace('/', '') ?? ''
+            const title = $('a', item).attr('title') ?? ''
+            const image = $('img', item).attr('src') ?? ''
+            results.push(
+                createMangaTile({
+                    id,
+                    image,
+                    title: createIconText({ text: this.encodeText(title) }),
+                })
+            )
+        }
+        return results
+    }
+
+    parseViewMore($: CheerioStatic): MangaTile[] {
+        const more: MangaTile[] = []
+        for (const item of $('.listupd .bsx').toArray()) {
+            const id    = $('a', item).attr('href')?.replace('https://flamescans.org/series/', '').replace('/', '') ?? ''
+            const title = $('a', item).attr('title') ?? ''
+            const image = $('img', item).attr('src') ?? ''
+            more.push(
+                createMangaTile({
+                    id,
+                    image,
+                    title: createIconText({ text: this.encodeText(title) }),
+                })
+            )
+        }
+        return more
+    }
+
+    parseHomeSections($: CheerioStatic, sectionCallback: (section: HomeSection) => void): void {
+        const section1 = createHomeSection({ id: '1', title: 'Popular', type: HomeSectionType.featured,})
+        const section2 = createHomeSection({ id: '2', title: 'Latest', type: HomeSectionType.singleRowNormal, view_more: true,})
+        const section3 = createHomeSection({ id: '3', title: 'Popular Titles', type: HomeSectionType.singleRowNormal, view_more: true,})
+
+        const featured: MangaTile[] = []
+        const popular : MangaTile[] = []
+        const latest  : MangaTile[] = []
+
+        const arrFeatured = $('.desktop-slide').toArray()
+        const arrPopular  = $('.pop-list-desktop .bsx').toArray()
+        const arrLatest   = $('.latest-updates .bsx').toArray()
+
+        for (const obj of arrFeatured) {
+            const id     = $(obj).attr('href')?.replace('https://flamescans.org/series/', '').replace('/', '') ?? ''
+            const title  = $('.tt', obj).text().trim()
+            const strImg = $('.bigbanner', obj).attr('style') ?? ''
+            const image  = strImg.substring(23, strImg.length - 3) ?? ''
+            featured.push(
+                createMangaTile({
+                    id,
+                    image,
+                    title: createIconText({ text: this.encodeText(title) }),
+                })
+            )
+        }
+        section1.items = featured
+        sectionCallback(section1)
+
+
+        for (const item of arrLatest) {
+            const id    = $('a', item).attr('href')?.replace('https://flamescans.org/series/', '').replace('/', '') ?? ''
+            const title = $('a', item).attr('title') ?? ''
+            const image = $('img', item).attr('src') ?? ''
+            latest.push(
+                createMangaTile({
+                    id,
+                    image,
+                    title: createIconText({ text: this.encodeText(title) }),
+                })
+            )
+        }
+
+        section2.items = latest
+        sectionCallback(section2)
+
+        for (const obj of arrPopular) {
+            const id      = $('a', obj).attr('href')?.replace('https://flamescans.org/series/', '').replace('/', '') ?? ''
+            const title   = $('a', obj).attr('title') ?? ''
+            const subText = $('.status', obj).text() ?? ''
+            const image   = $('img', obj).attr('src') ?? ''
+            popular.push(
+                createMangaTile({
+                    id,
+                    image,
+                    title: createIconText({ text: this.encodeText(title) }),
+                    subtitleText: createIconText({ text: subText }),
+                })
+            )
+        }
+        section3.items = popular
+        sectionCallback(section3)
+    }
+
+    encodeText(str: string) {
+        return str.replace(/&#([0-9]{1,4});/gi, function(_, numStr) {
+            var num = parseInt(numStr, 10)
+            return String.fromCharCode(num)
+        })
+    }
+}
