b513f53ad0612b370c17fd1cd163e616538a08ba
pandeynmn <namanpandey23@gmail.com>
2022-02-11 22:27:52 -0700

    MangaReader: Version increment
    MangaReader: Version increment

--- src/MangaReaderTo/MangaReaderTo.ts
+++ src/MangaReaderTo/MangaReaderTo.ts
@@ -20,7 +20,7 @@ import { ImageInterceptor } from './interceptors'
 const MANGAREADER_DOMAIN = 'https://mangareader.to'
 
 export const MangaReaderToInfo: SourceInfo = {
-    version: '2.0.0',
+    version: '2.0.1',
     name: 'MangaReaderTo',
     description: 'Extension that pulls manga from mangareader.to \nCanvas code by @Paper#1932',
     author: 'NmN | Ruii',
