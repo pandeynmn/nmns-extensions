83d62a35541b60b34bbc0d4428f677155fa8210a
pandeynmn <namanpandey23@gmail.com>
2022-05-28 22:00:36 +0000

    Deploying to gh-pages from @ pandeynmn/nmns-extensions@9bf4e9e008c426671aed492cc89ab26dfc081e51 🚀

--- mangareaderto-language-settings/MangaReaderTo/source.js
+++ mangareaderto-language-settings/MangaReaderTo/source.js
@@ -31977,35 +31977,8 @@ class MangaReaderTo extends paperback_extensions_common_1.Source {
             });
         });
     }
-    /**
-     * Parses a time string from a Madara source into a Date object.
-     * Copied from Madara.ts made by gamefuzzy
-     */
-    convertTime(timeAgo) {
-        var _a;
-        let time;
-        let trimmed = Number(((_a = /\d*/.exec(timeAgo)) !== null && _a !== void 0 ? _a : [])[0]);
-        trimmed = trimmed == 0 && timeAgo.includes('a') ? 1 : trimmed;
-        if (timeAgo.includes('mins') || timeAgo.includes('minutes') || timeAgo.includes('minute')) {
-            time = new Date(Date.now() - trimmed * 60000);
-        }
-        else if (timeAgo.includes('hours') || timeAgo.includes('hour')) {
-            time = new Date(Date.now() - trimmed * 3600000);
-        }
-        else if (timeAgo.includes('days') || timeAgo.includes('day')) {
-            time = new Date(Date.now() - trimmed * 86400000);
-        }
-        else if (timeAgo.includes('year') || timeAgo.includes('years')) {
-            time = new Date(Date.now() - trimmed * 31556952000);
-        }
-        else {
-            time = new Date(timeAgo);
-        }
-        return time;
-    }
 }
 exports.MangaReaderTo = MangaReaderTo;
-// npm i paperback-extensions-common@5.0.0-alpha.5 paperback-cli@2.0.0-alpha.13
 
 },{"./MangaReaderInterceptor":223,"./Parser":225,"./Settings":227,"./interceptors":230,"paperback-extensions-common":99}],225:[function(require,module,exports){
 "use strict";
