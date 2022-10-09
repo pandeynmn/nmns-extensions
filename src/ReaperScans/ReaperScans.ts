c88fa6b3dd522c92c803072b29b754c606ce6d46
pandeynmn <namanpandey23@gmail.com>
2022-10-09 01:08:14 -0700

    Incremented version counter

--- src/ReaperScans/ReaperScans.ts
+++ src/ReaperScans/ReaperScans.ts
@@ -21,7 +21,7 @@ import { Helper } from './helper'
 const REAPERSCANS_DOMAIN = 'https://reaperscans.com'
 
 export const ReaperScansInfo: SourceInfo = {
-    version: '3.0.0',
+    version: '3.0.1',
     name: 'ReaperScans',
     description: 'New Reaperscans source.',
     author: 'NmN',
