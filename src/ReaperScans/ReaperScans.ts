7ab9692134ff7b854d419f4d3e5f201789c3e47f
NMN <namanpandey23@gmail.com>
2022-12-08 18:55:00 -0700

    image url fix
    fix might break in the future, a quick fix till I check it.

--- src/ReaperScans/ReaperScans.ts
+++ src/ReaperScans/ReaperScans.ts
@@ -27,7 +27,7 @@ import {
 const REAPERSCANS_DOMAIN = 'https://reaperscans.com'
 
 export const ReaperScansInfo: SourceInfo = {
-    version: '3.0.10',
+    version: '3.0.11',
     name: 'ReaperScans',
     description: 'New Reaperscans source.',
     author: 'NmN',
