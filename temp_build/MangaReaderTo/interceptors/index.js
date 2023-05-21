c4249a57e033dfa03b7d2411ce123372d98dfa2d
NMN <56101163+pandeynmn@users.noreply.github.com>
2023-05-21 15:55:18 -0700

    fix(branch): removing temp build directory.

--- temp_build/MangaReaderTo/interceptors/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ImageInterceptor = void 0;
-var ImageInterceptor_1 = require("./ImageInterceptor");
-Object.defineProperty(exports, "ImageInterceptor", { enumerable: true, get: function () { return ImageInterceptor_1.ImageInterceptor; } });
