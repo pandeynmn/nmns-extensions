d43bda9b8506761e439546f38c748805e4ebe195
NMN <56101163+pandeynmn@users.noreply.github.com>
2023-06-16 10:54:14 -0700

    reaperscans: Version increment

--- src/ReaperScans/ReaperScans.ts
+++ src/ReaperScans/ReaperScans.ts
@@ -27,7 +27,7 @@ import {
 const REAPERSCANS_DOMAIN = 'https://reaperscans.com'
 
 export const ReaperScansInfo: SourceInfo = {
-    version: '3.0.13',
+    version: '3.0.14',
     name: 'ReaperScans',
     description: 'New Reaperscans source.',
     author: 'NmN',
